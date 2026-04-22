fc94eb6d56d41930cc92e0273e5640a2552c4528
Clément Josso <clement.josso@epfl.ch>
2026-04-22 20:30:33 +0200

    progress bar change

--- scripts/progress_bar.gd
+++ scripts/progress_bar.gd
@@ -1,17 +1,24 @@
-extends TextureProgressBar
+extends HBoxContainer
 
 @export var progress_low : CompressedTexture2D
 @export var progress_mid : CompressedTexture2D
 @export var progress_high : CompressedTexture2D
 
+@onready var bar : TextureProgressBar = $ProgressBar
+
+var value : int = 2:
+	set(val):
+		value = val
+		bar.value = val
+		print("coubeh")
 
 func _on_value_changed(val: float) -> void:
-	if val - min_value < (max_value - min_value)/3 :
-		texture_progress = progress_low
-	elif val - min_value > 2 * (max_value - min_value)/3:
-		texture_progress = progress_high
+	if val - bar.min_value < (bar.max_value - bar.min_value)/3 :
+		bar.texture_progress = progress_low
+	elif val - bar.min_value > 2 * (bar.max_value - bar.min_value)/3:
+		bar.texture_progress = progress_high
 	else:
-		texture_progress = progress_mid
+		bar.texture_progress = progress_mid
 	visible = true
 	get_tree().create_timer(3).timeout.connect(func() -> void:
 		visible = false)
