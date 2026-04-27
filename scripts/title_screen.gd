3b5c7c733d0fd82d01be55cbf4fde70148f625f2
LozanoCyrielle <cyrielle.lozano@epfl.ch>
2026-04-27 19:19:22 +0200

    New title screen

---
+++ scripts/title_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready() -> void:
+	$StartButton.grab_focus.call_deferred()
+	
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
