447fe2bf339d086941936e97bb0b6fc7c27e0a24
Clément Josso <clement.josso@epfl.ch>
2026-04-27 19:34:45 +0200

    update title

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,11 +1,14 @@
 extends Control
 
+const MAIN_SCENE : PackedScene = preload("res://scenes/main.tscn")
+
 func _ready() -> void:
 	$StartButton.grab_focus.call_deferred()
 	
+	
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_packed(MAIN_SCENE)
 
 
 func _on_exit_button_pressed() -> void:
