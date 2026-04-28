45bb3a461f52b3ab0d954d153ae28d74f387bab4
Clément Josso <clement.josso@epfl.ch>
2026-04-28 20:33:33 +0200

    feat: bar for timer

--- scripts/interaction_panel.gd
+++ scripts/interaction_panel.gd
@@ -4,8 +4,12 @@ class_name InteractionPanel
 signal answered(value: int)
 signal too_late
 
+var started: bool = false
+var timer_is_white: bool = true
+
 func _ready() -> void:
 	%AnswersContainer.get_child(0).grab_focus.call_deferred()
+	timer_is_white = %ProgressBarLeft.get("theme_override_styles/fill").bg_color == Color.WHITE
 
 
 func setup(tex: CompressedTexture2D, question: String,
@@ -23,17 +27,30 @@ func setup(tex: CompressedTexture2D, question: String,
 			answered.emit(i)
 			queue_free())
 	if timer_duration < 0:
-		%TimerLabel.visible = false
+		%TimerContainer.visible = false
 	else:
+		%ProgressBarLeft.max_value = timer_duration
+		%ProgressBarRight.max_value = timer_duration
 		$Timer.wait_time = timer_duration
 		$Timer.timeout.connect(func() -> void: 
 			get_tree().paused = false
 			too_late.emit()
 			queue_free())
 		$Timer.start()
+		started = true
 	get_tree().paused = true
 	visible = true
 
 
 func _process(_delta: float) -> void:
-	%TimerLabel.text = str(int($Timer.time_left)) + "s"
+	if started:
+		%ProgressBarLeft.value = $Timer.time_left
+		%ProgressBarRight.value = $Timer.time_left
+		if %ProgressBarLeft.value < %ProgressBarLeft.max_value/3 && timer_is_white:
+			%ProgressBarLeft.get("theme_override_styles/fill").bg_color = Color.from_rgba8(109, 12, 1, 255)
+			%ProgressBarRight.get("theme_override_styles/fill").bg_color = Color.from_rgba8(109, 12, 1, 255)
+			timer_is_white = false
+		elif %ProgressBarLeft.value >= %ProgressBarLeft.max_value/3 && !timer_is_white:
+			%ProgressBarLeft.get("theme_override_styles/fill").bg_color = Color.WHITE
+			%ProgressBarRight.get("theme_override_styles/fill").bg_color = Color.WHITE
+			timer_is_white = true
