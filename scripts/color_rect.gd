ed090132f0ba2bf66d647271f8e209fb58fbf880
lore <lorenzo.padrini@epfl.ch>
2026-05-06 20:26:56 +0200

    Added shader to project

--- scripts/color_rect.gd
+++ scripts/color_rect.gd
@@ -1,10 +1,11 @@
-extends ColorRect
-
+extends CanvasItem
 @onready var mat := material as ShaderMaterial
 
-#func _ready() -> void:
-#	Global.score_changed.connect(func(score: int) -> void:
-#		set_scene_temperature(score))
+func _ready() -> void:
+	Global.score_changed.connect(func(score: int) -> void:
+		change_color(score))
 
-func set_scene_temperature(value: float) -> void:
-	mat.set_shader_parameter("warmth", clamp(value, Global.SCORE_LOWER_BOUND, Global.SCORE_UPPER_BOUND))
+func change_color(score: float) -> void: 
+	var confidence: float = ((score - Global.SCORE_LOWER_BOUND) / (Global.SCORE_UPPER_BOUND - Global.SCORE_LOWER_BOUND));
+	if mat != null: 
+		mat.set_shader_parameter("confidence", confidence)
